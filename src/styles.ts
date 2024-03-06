cc597f501410f1f643aaf07c2db7a61739e93211
Piriya <piriyalapa@gmail.com>
2024-03-06 21:44:50 +0700

    update and try todeply

---
+++ src/styles.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle<{}>`
+    * {
+        margin:0;
+        padding:0;
+        box-sizing: border-box;
+    }
+
+    body {
+        // Mobile
+        @media screen and (max-width:767px){
+            //
+
+        }
+
+        // Tablet
+        @media screen and (min-width:768px) and (max-width:1023px){
+            // background-color:orange;
+            /* padding: 3% 5%; */
+        }
+
+        // Labtop
+        @media screen and (min-width:1024px) and (max-width:1439px){
+            // background-color:yellow;
+            /* padding: 5% 5%; */
+        }
+
+        // Desktop
+        @media screen and (min-width:1440px){
+            // background-color:red;
+            /* padding:5% 5%; */
+        }
+    }
+`;
+
+export default GlobalStyles;
