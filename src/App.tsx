043b1ae85465fb88cd6ef63870e5921cb301c8fa
Piriya <piriyalapa@gmail.com>
2024-03-07 19:34:58 +0700

    update and try to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
+       <GlobalStyles /> 
         <h1>Hello meet-lnading-page</h1>       
       </div>
     );
