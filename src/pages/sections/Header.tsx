9d2ed77124c83f721a54d85c412123a6c7baa8d6
PiriyaLapa <piriyalapa@gmail.com>
2024-03-15 11:59:20 +0700

    added galleryContent card

--- src/pages/sections/Header.tsx
+++ src/pages/sections/Header.tsx
@@ -3,7 +3,6 @@ import { StyledHeader } from "../../styles/styles";
 import Image from "../../components/image/Image";
 import urlImg from "../../assets/tablet/image-hero.webp";
 import CardHeader from "../../components/cards/CardHeader";
-// import Vector1 from "../../components/vectors/Vector1";
 
 export default class Header extends Component {
   render(): ReactNode {
@@ -11,7 +10,6 @@ export default class Header extends Component {
       <StyledHeader>
         <Image urlImg={urlImg} />
         <CardHeader />
-        {/* <Vector1 /> */}
       </StyledHeader>
     );
   }
